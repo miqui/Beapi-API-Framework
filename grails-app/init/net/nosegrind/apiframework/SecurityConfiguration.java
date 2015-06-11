bd3bcb2476843456049efe9c0a39f18cf09da615
Owen Rubel <orubel@gmail.com>
2015-06-11 09:47:03 -0700

    working plugin; saving and testing

--- grails-app/init/net/nosegrind/apiframework/SecurityConfiguration.java
+++ grails-app/init/net/nosegrind/apiframework/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package net.nosegrind.apiframework;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
--- spring/resources.groovy
+++ spring/resources.groovy
@@ -1,6 +1,6 @@
 // Place your Spring DSL code here
-import net.nosegrind.apiframework.GormUserDetailsService
-import net.nosegrind.apiframework.SecurityConfiguration
+//import net.nosegrind.apiframework.GormUserDetailsService
+//import net.nosegrind.apiframework.SecurityConfiguration
 
 beans = {
     webSecurityConfiguration(SecurityConfiguration)
