263a027efc55d9b200ac72745e5ede3d2c6edbaa
Owen Rubel <orubel@gmail.com>
2016-03-12 07:17:53 -0800

    saving changes for gh-pages

--- grails-app/conf/spring/resources.groovy
+++
@@ -1,8 +0,0 @@
-// Place your Spring DSL code here
-import net.nosegrind.apiframework.GormUserDetailsService
-import net.nosegrind.apiframework.SecurityConfiguration
-
-beans = {
-    webSecurityConfiguration(SecurityConfiguration)
-    userDetailsService(GormUserDetailsService)
-}
