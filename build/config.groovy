a289a8fe5dfd17a6892fcfa439815deb211ddfab
Owen Rubel <orubel@gmail.com>
2016-03-21 15:49:37 -0700

    Delete config.groovy

--- build/config.groovy
+++
@@ -1,8 +0,0 @@
-
-withConfig(configuration) {
-    inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '0.1-SNAPSHOT')
-        classNode.putNodeMetaData('projectName', 'api-framework')
-        classNode.putNodeMetaData('isPlugin', 'true')
-    }
-}
