f59bd20413c446321b8b4ead93c8fd1afbe65276
Owen Rubel <orubel@gmail.com>
2016-03-22 09:25:02 -0700

    saving changes

--- build/config.groovy
+++ build/config.groovy
@@ -1,7 +1,7 @@
 
 withConfig(configuration) {
     inline(phase: 'CONVERSION') { source, context, classNode ->
-        classNode.putNodeMetaData('projectVersion', '0.1-SNAPSHOT')
+        classNode.putNodeMetaData('projectVersion', '0.1')
         classNode.putNodeMetaData('projectName', 'api-framework')
         classNode.putNodeMetaData('isPlugin', 'true')
     }
