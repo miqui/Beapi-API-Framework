f0fe35175b29da3992d0f05434d1ae6e9cbfadfc
Owen Rubel <orubel@gmail.com>
2017-02-16 14:08:57 -0800

    Update ApiParams.groovy

--- src/main/groovy/net/nosegrind/apiframework/ApiParams.groovy
+++ src/main/groovy/net/nosegrind/apiframework/ApiParams.groovy
@@ -1,6 +1,13 @@
-/* ****************************************************************************
- * Copyright 2014 Owen Rubel
- *****************************************************************************/
+/*
+ * Academic Free License ("AFL") v. 3.0
+ * Copyright 2014-2017 Owen Rubel
+ *
+ * IO State (tm) Owen Rubel 2014
+ * API Chaining (tm) Owen Rubel 2013
+ *
+ *   https://opensource.org/licenses/AFL-3.0
+ */
+
 package net.nosegrind.apiframework
 
 
